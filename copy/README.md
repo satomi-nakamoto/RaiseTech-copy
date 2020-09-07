c3e1015b5efdf13bdccb5bdedf71fcae09409a08
satomi-nakamoto <64627738+satomi-nakamoto@users.noreply.github.com>
2020-09-07 15:46:27 +0900

    Add files via upload
    模写課題

---
+++ copy/README.md
@@ -0,0 +1,1 @@
+＃RaiseTech_copy
---
+++ copy/js/javascript.js
@@ -0,0 +1,15 @@
+function myFunc() {
+  document.querySelector('.raisetech').classList.toggle('is_open');
+}
+
+function sub1Func() {
+  document.querySelector('.raisetech').classList.toggle('is_sub1_open');
+}
+
+function sub2Func() {
+  document.querySelector('.raisetech').classList.toggle('is_sub2_open');
+}
+
+function sub3Func() {
+  document.querySelector('.raisetech').classList.toggle('is_sub3_open');
+}
